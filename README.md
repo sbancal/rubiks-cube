8fe5a0bce92a993b9157c807da793e0a61a98a6d
Samuel Bancal <a.s.bancal@bluewin.ch>
2015-12-28 14:54:21 +0100

    2015-12-28 +solve_rubik.py
    +parse_arguments with positional argument "filename"

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 README
+
+This is intended to resolve Rubik's cubes of n*n*n elements
+
+Example input can be read in random1.txt
+
+You can solve it with :
+
+~~~ bash
+./solve_rubik.py random1.txt
+~~~
---
+++ solve_rubik.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Bancal Samuel
+
+"""
+Solves rubik's cube given in parameter
+"""
+
+import pprint
+import argparse
+
+def parse_arguments():
+    """
+        Parse arguments and return them
+    """
+    options = {
+        "filename": None,
+    }
+    parser = argparse.ArgumentParser(description="Solves rubik's cube")
+    parser.add_argument("filename", metavar="f", type=str, help="a filename containing a Rubik's cube to solve")
+    args = parser.parse_args()
+    options["filename"] = args.filename
+    return options
+
+if __name__ == "__main__":
+    print("Going to solve Rubik's cube ...")
+    options = parse_arguments()
+    pprint.pprint(options)
+    print("Done. (well not yet ;)")
