1fd604792b0dda950c8368c508d881766eb69c64
Samuel Bancal <sam.bancal@gmail.com>
2017-04-10 23:30:54 +0200

    2017-04-10 +evaluator.give_a_note
    evaluator.py
    * Don't rely on eval_matrices but on eval_indexes (faster)
    + give_a_note returns a note based on the different eval_indexes pre-defined
    + give_a_note raises SolvedCubeException when giving a note to a solved cube
    + tests

--- evaluator.py
+++ evaluator.py
@@ -5,19 +5,51 @@
 """
 Provides Evaluator class that will return notes which can be compared then.
 Comparing notes between Cubes should indicate if one is closer to be resolved.
+
+Notes are given in a tuple: (a, b, c)
+`a` has more weight than `b` which has more weight than `c`
+
+In case the cube is solved, SolvedCubeException is raised.
 """
 
 import numpy as np
+
+class SolvedCubeException(Exception):
+    pass
 
 class Evaluator():
     def __init__(self, n):
         self.n = n
 
-        #Prepare eval_matrices
+        # Prepare eval_indexes
         first_i = int((n-1)/2)
         last_i = int((n)/2)
-        self.eval_matrices = []
-        for i in range(int((n-1)/2)+1):
-            mat = np.zeros((n,n), dtype=np.bool)
-            mat[first_i-i:last_i+i+1,first_i-i:last_i+i+1] = True
-            self.eval_matrices.append(mat)
+        self.eval_indexes = []
+        if self.n % 2 == 0:
+            start = 0
+        else:
+            start = 1
+        for i in range(start, int((n-1)/2)+1):
+            self.eval_indexes.append((first_i-i,last_i+i+1))
+
+        self.color_index = first_i
+        self.nb_stickers = n*n*6
+
+    def give_a_note(self, cube):
+        """
+        return a tuple (a, b, ...) where
+            a is the nb of good stickers summed on all faces according to self.eval_indexes[0]
+            b is the nb of good stickers summed on all faces according to self.eval_indexes[1]
+            ...
+        It will raise SolvedCubeException when cube is 100% solved
+        """
+        note = []
+        for current_index in self.eval_indexes:
+            current_index_note = 0
+            for face in cube.faces.values():
+                nb_good_stickers = len(np.where(face[current_index[0]:current_index[1], current_index[0]:current_index[1]]==face[self.color_index, self.color_index])[0])
+                current_index_note += nb_good_stickers
+            note.append(current_index_note)
+        if note[-1] == self.nb_stickers:
+            raise SolvedCubeException()
+        return tuple(note)
--- test_evaluator.py
+++ test_evaluator.py
@@ -1,83 +1,52 @@
-import numpy as np
 import unittest
-from evaluator import Evaluator
+import numpy as np
+from model import Cube
+from evaluator import Evaluator, SolvedCubeException
 
-class TestEvaluator(unittest.TestCase):
-    def test_eval_matrices_2x2x2(self):
+class TestEvaluatorEvalMatrices(unittest.TestCase):
+    def test_eval_indexes_2x2x2(self):
         evaluator = Evaluator(2)
-        self.assertEqual(len(evaluator.eval_matrices), 1)
-        self.assertEqual(evaluator.eval_matrices[0].tolist(),
-            [[ True,  True], [True,  True]])
+        self.assertEqual(len(evaluator.eval_indexes), 1)
+        self.assertEqual(evaluator.eval_indexes[0], (0, 2))
 
-    def test_eval_matrices_3x3x3(self):
+    def test_eval_indexes_3x3x3(self):
         evaluator = Evaluator(3)
-        self.assertEqual(len(evaluator.eval_matrices), 2)
-        self.assertEqual(evaluator.eval_matrices[0].tolist(),
-            [[False, False, False],
-             [False,  True, False],
-             [False, False, False]])
-        self.assertEqual(evaluator.eval_matrices[1].tolist(),
-            [[ True,  True,  True],
-             [ True,  True,  True],
-             [ True,  True,  True]])
+        self.assertEqual(len(evaluator.eval_indexes), 1)
+        self.assertEqual(evaluator.eval_indexes[0], (0, 3))
 
-    def test_eval_matrices_4x4x4(self):
+    def test_eval_indexes_4x4x4(self):
         evaluator = Evaluator(4)
-        self.assertEqual(len(evaluator.eval_matrices), 2)
-        self.assertEqual(evaluator.eval_matrices[0].tolist(),
-            [[False, False, False, False],
-             [False, True, True, False],
-             [False, True, True, False],
-             [False, False, False, False]])
-        self.assertEqual(evaluator.eval_matrices[1].tolist(),
-            [[True, True, True, True],
-             [True, True, True, True],
-             [True, True, True, True],
-             [True, True, True, True]])
+        self.assertEqual(len(evaluator.eval_indexes), 2)
+        self.assertEqual(evaluator.eval_indexes[0], (1, 3))
+        self.assertEqual(evaluator.eval_indexes[1], (0, 4))
 
-    def test_eval_matrices_5x5x5(self):
+    def test_eval_indexes_5x5x5(self):
         evaluator = Evaluator(5)
-        self.assertEqual(len(evaluator.eval_matrices), 3)
-        self.assertEqual(evaluator.eval_matrices[0].tolist(),
-            [[False, False, False, False, False],
-             [False, False, False, False, False],
-             [False, False, True, False, False],
-             [False, False, False, False, False],
-             [False, False, False, False, False]])
-        self.assertEqual(evaluator.eval_matrices[1].tolist(),
-            [[False, False, False, False, False],
-             [False, True, True, True, False],
-             [False, True, True, True, False],
-             [False, True, True, True, False],
-             [False, False, False, False, False]])
-        self.assertEqual(evaluator.eval_matrices[2].tolist(),
-            [[True, True, True, True, True],
-             [True, True, True, True, True],
-             [True, True, True, True, True],
-             [True, True, True, True, True],
-             [True, True, True, True, True]])
+        self.assertEqual(len(evaluator.eval_indexes), 2)
+        self.assertEqual(evaluator.eval_indexes[0], (1, 4))
+        self.assertEqual(evaluator.eval_indexes[1], (0, 5))
 
-    def test_eval_matrices_6x6x6(self):
+    def test_eval_indexes_6x6x6(self):
         evaluator = Evaluator(6)
-        self.assertEqual(len(evaluator.eval_matrices), 3)
-        self.assertEqual(evaluator.eval_matrices[0].tolist(),
-            [[False, False, False, False, False, False],
-             [False, False, False, False, False, False],
-             [False, False, True, True, False, False],
-             [False, False, True, True, False, False],
-             [False, False, False, False, False, False],
-             [False, False, False, False, False, False]])
-        self.assertEqual(evaluator.eval_matrices[1].tolist(),
-            [[False, False, False, False, False, False],
-             [False, True, True, True, True, False],
-             [False, True, True, True, True, False],
-             [False, True, True, True, True, False],
-             [False, True, True, True, True, False],
-             [False, False, False, False, False, False]])
-        self.assertEqual(evaluator.eval_matrices[2].tolist(),
-            [[True, True, True, True, True, True],
-             [True, True, True, True, True, True],
-             [True, True, True, True, True, True],
-             [True, True, True, True, True, True],
-             [True, True, True, True, True, True],
-             [True, True, True, True, True, True]])
+        self.assertEqual(len(evaluator.eval_indexes), 3)
+        self.assertEqual(evaluator.eval_indexes[0], (2, 4))
+        self.assertEqual(evaluator.eval_indexes[1], (1, 5))
+        self.assertEqual(evaluator.eval_indexes[2], (0, 6))
+
+
+class TestEvaluatorGiveANote(unittest.TestCase):
+    def test_give_a_note_on_random1(self):
+        cube = Cube('random1.txt')
+        evaluator = Evaluator(3)
+        self.assertEqual(evaluator.give_a_note(cube), (18,))
+
+    def test_give_a_note_on_6_random1(self):
+        cube = Cube('6_random1.txt')
+        evaluator = Evaluator(6)
+        self.assertEqual(evaluator.give_a_note(cube), (12, 35, 84))
+
+    def test_give_a_note_on_3x3x3_solved(self):
+        cube = Cube('3x3x3_solved.txt')
+        evaluator = Evaluator(3)
+        with self.assertRaises(SolvedCubeException):
+            evaluator.give_a_note(cube)
